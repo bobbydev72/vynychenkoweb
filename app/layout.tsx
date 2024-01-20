82983ec2a3d50d67f389eaf5de0cb895ce2f8fbf
Dmitry Bobryshev <dmitry.b25@MacBook-Pro-dmitryb25.local>
2024-01-20 21:45:00 +0100

    :sparkles:  feat(global): create intro page

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'vynychenko.com - wedding, baloons, party',
-  description: '-',
+  title: 'Студія декору "Я Можу ВСЕ"',
+  description: 'Студія декору "Я Можу ВСЕ"',
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,40 @@
 export default function Home() {
-  return <></>;
+  return (
+    <section className="relative h-screen overflow-hidden before:absolute before:inset-0 before:w-full before:h-full before:bg-white/80 before:z-40">
+      <div className="container mx-auto">
+        <div className="relative flex flex-col items-center justify-center h-screen my-auto text-center z-50">
+          <h2 className="text-5xl mb-12">
+            Студія декору{' '}
+            <span className="text-blue-600 font-bold">&#8221;Я Можу ВСЕ&#8221;</span>
+          </h2>
+          <div className="w-full max-w-[900px] [&>*:nth-child(n+2)]:mt-7">
+            <p>Ласкаво просимо на наш веб-сайт!</p>
+            <p>
+              Ми раді вас вітати на нашому порталі, де розгортається невимовно цікавий
+              процес розробки. Наразі ми працюємо над створенням унікального простору,
+              який відповідатиме вашим очікуванням і вимогам.
+            </p>
+            <p>
+              Щоб забезпечити вам найкращий досвід користування, наша команда фахівців
+              пильно вдосконалює кожен аспект сайту. Будь ласка, залишайтеся на
+              зв&apos;язку та слідкуйте за нашими оновленнями, оскільки ми плануємо
+              додавати нові функції та зміни, щоб зробити ваше перебування тут ще більш
+              комфортним та захопливим.
+            </p>
+            <p>
+              Дякуємо за ваше терпіння та розуміння. Завітайте до нас знову найближчим
+              часом, коли наш сайт буде повністю готовий подарувати вам новий рівень
+              віртуального досвіду!
+            </p>
+          </div>
+        </div>
+      </div>
+      <video
+        src="/video/introducing.mp4"
+        muted
+        autoPlay
+        className="w-full h-full object-cover absolute inset-0"
+      />
+    </section>
+  );
 }
